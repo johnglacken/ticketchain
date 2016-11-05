ce31244a8a67a1f55eb3c0b5049e21a15f35b5c0
Aidan Killeen <aidan@Admins-MacBook-Pro.local>
2016-11-05 20:27:53 +0000

    moved to functions.js

--- backend/app/javascripts/functions.js
+++ backend/app/javascripts/functions.js
@@ -1,11 +1,43 @@
 var web3 = require('web3');
+var accounts;
 
 $(document).on("ready", function(){
+  web3 = new Web3(new Web3.providers.HttpProvider("http://blockhack.northeurope.cloudapp.azure.com:8545"));
 
-web3 = new Web3(new Web3.providers.HttpProvider("http://blockhack.northeurope.cloudapp.azure.com:8545"));
+  populateAccounts();
+});
+
+function populateAccounts() {
+
   web3.eth.getAccounts(function(err, accs) {
 
-    alert(accs[0]);
+    accounts = accs;
+
+    $.each(accounts, function(index, accounts){
+      $("#accounts").append("<tr><td>" + acc + "</td><td>" + web3.fromWei(web3.eth.getBalance(acc)) + "</td></tr>");
+    });
 
   });
-});
+
+}
+
+function transferToAccount(fromAccount, toAccount, amount) {
+
+
+    web3.eth.sendTransaction({from:fromAccount,
+                         to:toAccount,
+                         value: web3.toWei(amount, "ether"),
+                         data:"caoimhe"
+                       }, function(err, address){
+                         console.log("sendTransaction complete");
+                         if (!err) {
+                           console.log("address:" + address);
+                           var t = web3.eth.getTransaction(address);
+                           displayTransaction(t);
+                           return t;
+                         } else {
+                           console.log("error:" + err);
+                           return false;
+                         }
+                       });
+}
