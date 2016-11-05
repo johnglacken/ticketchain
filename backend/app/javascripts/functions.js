992fc2cf5b2c2400a425e8b8b355f9af984559c2
Aidan Killeen <aidan@Admins-MacBook-Pro.local>
2016-11-05 20:14:15 +0000

    Added table with accounts and balances

--- backend/app/javascripts/functions.js
+++ backend/app/javascripts/functions.js
@@ -2,9 +2,10 @@ var web3 = require('web3');
 
 $(document).on("ready", function(){
 
-  //web3.eth.getAccounts(function(err, accs) {
+web3 = new Web3(new Web3.providers.HttpProvider("http://blockhack.northeurope.cloudapp.azure.com:8545"));
+  web3.eth.getAccounts(function(err, accs) {
 
-  //  alert(accs[0]);
+    alert(accs[0]);
 
-  //});
+  });
 });
